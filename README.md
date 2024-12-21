f28abfabaa68ed9a2390b5b3085eff8660106de7
Idan Z <127608063+nad1z@users.noreply.github.com>
2024-12-21 20:19:25 +0000

    working mvp

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Prioritize
 
 ## What is Prioritize.io?
-An easy space/hub to help PM's prioritize thier tasks/user storys/feature requests.
-The task is to take all the most used prioratization tools and put them all under the same roof.
+An easy space/hub to help PM's prioritize their tasks/user story/feature requests.
+The task is to take all the most used prioritization tools and put them all under the same roof.
 
--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
-console.log("hey");
-
-
+//RICE Calculator 
 function calculateRice() {
+  var featureOneName = document.getElementsByName("featureOneReach")[0].textContent;
   var featureOneReach = document.getElementsByName("featureOneReach")[0].value;
   var featureOneImpact = document.getElementsByName("featureOneImpact")[0].value;
   var featureOneConfidence = document.getElementsByName("featureOneConfidence")[0].value;
   var featureOneEffort = document.getElementsByName("featureOneEffort")[0].value;
-  var featureOneRiceScore = document.get()
-  console.log(featureOneImpact);
-
+  var featureOneRiceScore = ((featureOneReach*featureOneImpact*featureOneConfidence)/featureOneEffort)
+  var featureOneRiceScore = document.getElementById("featureOneRiceScore").innerHTML = Math.round(featureOneRiceScore);
 }
-
 
 //Fotters year auto update
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
