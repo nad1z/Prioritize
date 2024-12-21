28d89f7dca4031593929a626c29fd64ec16403c6
Idan Z <127608063+nad1z@users.noreply.github.com>
2024-12-21 12:57:41 +0000

    initial calculator function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
+//inputs
+var featureOneReach = document.getElementsByClass("featureOneReach");
+var featureOneImpact = document.getElementsByClass("featureOneImpact");
+let featureOneRiceScore = document.querySelector("featureOneRiceScore");
+
+//results 
+function calculateRice() {
+    var featureOneRiceScore = Number(featureOneReach*featureOneImpact)
+    document.getElementsById("featureOneRiceScore").innerHTML = "featureOneRiceScore"
+    console.log(featureOneRiceScore)
+}
+onclick("button") = function()
+
 //Fotters year auto update
 document.getElementById("currentYear").innerHTML = (new Date()).getFullYear()
