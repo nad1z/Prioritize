82d2921aedd66278f66ad8c4430ae0eadd934ec8
Idan Z <127608063+nad1z@users.noreply.github.com>
2024-12-27 21:38:36 +0000

    made sorting and current results show up

--- js/index.js
+++ js/index.js
@@ -1,88 +1,80 @@
-//RICE Calculator
+
 function calculateRice() {
-  let featureName = document.getElementsByName("featureReach")[0].textContent;
+  //RICE Calculator
+  let featureName = document.getElementsByName("featureName")[0].value;
   let featureReach = document.getElementsByName("featureReach")[0].value;
   let featureImpact = document.getElementsByName("featureImpact")[0].value;
   let featureConfidence = document.getElementsByName("featureConfidence")[0].value;
   let featureEffort = document.getElementsByName("featureEffort")[0].value;
   let featureRiceScore = (featureReach * featureImpact * featureConfidence * 100) / featureEffort;
-  let featureRiceResult = (document.getElementById("featureRiceScore").innerHTML = Math.round(featureRiceScore));
+  let featureRiceResult = (document.getElementById("featureRiceResult").innerHTML = Math.round(featureRiceScore));
+    if (featureRiceScore < 1) {
+    document.getElementById("featureRiceResult").innerText = "Score is too low :(";
+  } else if (isNaN(featureRiceScore)) {
+    document.getElementById("featureRiceResult").innerText = "Please insert numbers";
+  }
+
+// defining dropdown options values
+let featureImpact1 = document.getElementById("featureImpact"); 
+let featureConfidence1 = document.getElementById("featureConfidence");
+let featureImpactText = featureImpact1.options[featureImpact1.selectedIndex].textContent;
+let featureConfidenceText = featureConfidence1.options[featureConfidence1.selectedIndex].textContent;
+
+  //adding a table row and cells for each calculation
+let tableRice = document.getElementById("riceTable");
+let newRow = tableRice.insertRow(2);
+let cell1 = newRow.insertCell(0);
+let cell2 = newRow.insertCell(1);
+let cell3 = newRow.insertCell(2);
+let cell4 = newRow.insertCell(3);
+let cell5 = newRow.insertCell(4);
+let cell6 = newRow.insertCell(5);
+
+// inserting to cells
+cell1.innerText = featureName;
+cell2.innerHTML = featureReach;
+cell3.innerHTML = featureImpactText;
+cell4.innerHTML = featureConfidenceText;
+cell5.innerHTML = featureEffort;
+cell6.innerHTML = featureRiceResult;
 }
 
-// //adding a table row for each calculations
-// var riceTable = document.getElementById("riceTable");
-// console.log(riceTable)
+//sorting button
+function sortTable() {
+  let table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("riceTable");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 2; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[5];
+      y = rows[i + 1].getElementsByTagName("TD")[5];
+      if (Number(x.innerHTML) > Number(y.innerHTML)) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 
-// // Create an empty <tr> element and add it to the 1st position of the table:
-// // pull the current number of rows - create a loop to check it and a var that will enter the free space to create the insert row each time the user presses calculate
-// var newRow = table.insertRow(0);
-
-// // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-// var cell1 = newRow.insertCell(0);
-// var cell2 = newRow.insertCell(1);
-
-// // Add some text to the new cells:
-// cell1.innerHTML = "NEW CELL1";
-// cell2.innerHTML = "NEW CELL2";
-
-// //adding the data for the added table
-
-
-
-// //sorting button
-// <script>
-// function sortTable() {
-//   var table, rows, switching, i, x, y, shouldSwitch;
-//   table = document.getElementById("myTable");
-//   switching = true;
-//   /*Make a loop that will continue until
-//   no switching has been done:*/
-//   while (switching) {
-//     //start by saying: no switching is done:
-//     switching = false;
-//     rows = table.rows;
-//     /*Loop through all table rows (except the
-//     first, which contains table headers):*/
-//     for (i = 1; i < (rows.length - 1); i++) {
-//       //start by saying there should be no switching:
-//       shouldSwitch = false;
-//       /*Get the two elements you want to compare,
-//       one from current row and one from the next:*/
-//       x = rows[i].getElementsByTagName("TD")[0];
-//       y = rows[i + 1].getElementsByTagName("TD")[0];
-//       //check if the two rows should switch place:
-//       if (Number(x.innerHTML) > Number(y.innerHTML)) {
-//         //if so, mark as a switch and break the loop:
-//         shouldSwitch = true;
-//         break;
-//       }
-//     }
-//     if (shouldSwitch) {
-//       /*If a switch has been marked, make the switch
-//       and mark that a switch has been done:*/
-//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//       switching = true;
-//     }
-//   }
-// }
-// </script> 
-
+//clear all
+// <button onclick="document.getElementById('myInput').value = ''">Clear input field</button>
+// <input type="text" value="Blabla" id="myInput"></input>
+// or 
+// document.querySelectorAll(".axs").forEach(x => x.value = '');
 
 //export to excel
-// <button id="btnExport" onclick="exportReportToExcel(this)">Export HTML Table</button>
-
-// <script type="text/javascript">
-// function exportReportToExcel() {
-// let table = document.getElementsByID("table");
-// TableToExcel.convert(table[0], {
-// name: `file.xlsx`,
-// sheet: {
-// name: 'Sheet 1'
+// <button id="btnExport" onclick="exportTableToExcel(this)">Export 🖫</button> //html button
+// function exportTableToExcel() {
+//   let table1 = document.getElementById("riceTable");
+//   TableToExcel.convert(table1[0], {name: `MyRiceTable.xlsx`,sheet: {name: 'Prioritize.io'}});
 // }
-// });
-// }
-// </script>
-
 
 //Fotters year auto update
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
